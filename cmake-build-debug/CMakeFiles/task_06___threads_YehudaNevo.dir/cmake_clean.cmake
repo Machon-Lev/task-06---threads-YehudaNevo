72075115ed88af86cd14bf50adc0aedaac463933
YehudaNevo <ynevo1948@gmail.com>
2023-07-02 16:50:25 +0300

    init

---
+++ cmake-build-debug/CMakeFiles/task_06___threads_YehudaNevo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/task_06___threads_YehudaNevo.dir/main.cpp.o"
+  "task_06___threads_YehudaNevo"
+  "task_06___threads_YehudaNevo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/task_06___threads_YehudaNevo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
